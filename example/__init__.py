8ab287cc2f0cad3b9b9b58f1e72ba370a7e6b8a7
Homekeeper <homekeeper89@gmail.com>
2019-07-10 23:15:43 +0900

    [new] user 부분 마무리, article 추가

--- example/__init__.py
+++ example/__init__.py
@@ -4,7 +4,9 @@ from flask import Flask
 
 def create_app(config):
     from .view import api_user
+    from .view import api_article
     
     app = Flask(__name__)
     app.register_blueprint(api_user)
+    app.register_blueprint(api_article)
     return app
--- mock_database.py
+++ mock_database.py
@@ -4,8 +4,16 @@ def mock_database_write(data, file_name):
     json_data = mock_database_read(file_name)
     IDX = len(json_data) + 1
     json_data[IDX] = data
-    with open(file_name, 'w') as cj:
-        json.dump(json_data, cj, ensure_ascii=False, indent=4)
+    
+    try:
+        with open(file_name, 'w') as cj:
+            json.dump(json_data, cj, ensure_ascii=False, indent=4)
+    except IOError as ie:
+        print(ie)
+        return False
+
+    return True
+    
 
 def mock_database_read(file_name):
     data = {}
