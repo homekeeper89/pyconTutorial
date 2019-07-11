ee05bbd2e74b7bf98d54d217d6d702e466faba60
Homekeeper <homekeeper89@gmail.com>
2019-07-12 00:21:17 +0900

    [fix] 팩토리 패턴 부분 환경 설정 추가

--- basic/flask_tutorial/cp_2_routing.py
+++ basic/flask_tutorial/cp_2_routing.py
@@ -1,7 +1,5 @@
 # copyright : homekeeper89@gmail.com
 # 플라스크 기본 구동 방법을 배우는 곳
-#
-#
 
 from flask import Flask
 
--- basic/flask_tutorial/cp_4_create_app.py
+++ basic/flask_tutorial/cp_4_create_app.py
@@ -3,7 +3,8 @@ from flask import Flask
 
 def create_app(config_filename):
     app = Flask(__name__)
-
+    #app.config.from_pyfile(config_filename)
+    app.debug = True if config_filename == 'dev' else False
     from cp_3_view import my_page
     from cp_3_view import my_store
     app.register_blueprint(my_page)
