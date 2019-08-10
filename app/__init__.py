1d82b324c7296a04ed3037333fde37c87f73fba7
김장훈 <homekeeper@gdnet.co.kr>
2019-08-10 16:12:17 +0900

    [new] demo 내용 수정

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-# app/__init__.py
-# 해당 폴더경로에서 공통적으로 쓰이는 부분을 전부 여기에 넣을꺼에요.
-# 공통적으로쓰인다는 것은 모듈 또는 함수 등이 됩니다.
-
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-    return app 
--- server_run.py
+++
@@ -1,9 +0,0 @@
-# pyconTutorial/server_run.py
-# 서버를 실제 런 하는 부분
-
-from app import create_app
-
-app = create_app()
-
-if __name__ =='__main__':
-    app.run()
